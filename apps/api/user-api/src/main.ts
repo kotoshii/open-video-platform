5c869b614ffe1cd8c09d5b0dffc45e710e9cc7b9
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-10 01:58:53 +0300

    feat(user-api): OVP-29 Set up swagger docs config.

--- apps/api/user-api/src/main.ts
+++ apps/api/user-api/src/main.ts
@@ -1,5 +1,6 @@
 import { ClassSerializerInterceptor, Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "~src/app.module";
 import { LoggingInterceptor } from "~src/common/interceptors/logging.interceptor";
@@ -25,6 +26,20 @@ async function bootstrap() {
 
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 	app.useGlobalInterceptors(new LoggingInterceptor());
+
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle("User API Specification")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup("docs/api", app, document, {
+		swaggerOptions: {
+			tagsSorter: "alpha",
+			operationsSorter: "method",
+		},
+	});
 
 	await app.listen(appConfig.port);
 
