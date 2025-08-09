08789be899e4047bb47308a08726475caaf6de05
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-10 01:53:11 +0300

    feat(user-api): OVP-25 Enable global interceptors and validation pipes; enable global "api" prefix; enable CORS config.

--- apps/api/user-api/src/main.ts
+++ apps/api/user-api/src/main.ts
@@ -1,9 +1,33 @@
-import { NestFactory } from "@nestjs/core";
+import { ClassSerializerInterceptor, Logger, ValidationPipe } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
 
 import { AppModule } from "~src/app.module";
+import { LoggingInterceptor } from "~src/common/interceptors/logging.interceptor";
+import { AppConfig } from "~src/config/providers/app.config";
+
+const logger = new Logger("bootstrap");
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+	const appConfig = app.get(AppConfig);
+
+	app.setGlobalPrefix("api");
+
+	app.enableCors({ origin: appConfig.corsDomains });
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			transformOptions: { enableImplicitConversion: true },
+		}),
+	);
+
+	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+	app.useGlobalInterceptors(new LoggingInterceptor());
+
+	await app.listen(appConfig.port);
+
+	logger.debug(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
