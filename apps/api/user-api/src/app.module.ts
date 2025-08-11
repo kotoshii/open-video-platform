674b25e0c39922972a074781ba7c88057122cb25
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-11 18:44:03 +0300

    feat(user-api): Set up Kysely module database connection.

--- apps/api/user-api/src/app.module.ts
+++ apps/api/user-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from "@nestjs/common";
+import { CamelCasePlugin, DeduplicateJoinsPlugin, PostgresDialect } from "kysely";
+import { KyselyModule } from "nestjs-kysely";
+import { Pool } from "pg";
 
 import { ConfigModule } from "~src/config/config.module";
+import { DatabaseConfig } from "~src/config/providers/database.config";
 import { UsersModule } from "~src/users/users.module";
 
 @Module({
-	imports: [ConfigModule, UsersModule],
+	imports: [
+		ConfigModule,
+		KyselyModule.forRootAsync({
+			useFactory: (databaseConfig: DatabaseConfig) => ({
+				dialect: new PostgresDialect({
+					pool: new Pool({ connectionString: databaseConfig.databaseUrl }),
+				}),
+				plugins: [new DeduplicateJoinsPlugin(), new CamelCasePlugin()],
+			}),
+			inject: [DatabaseConfig],
+		}),
+		UsersModule,
+	],
 })
 export class AppModule {}
