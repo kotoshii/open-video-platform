3d9ef3046a6524acd6a68134c0472cc155871803
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-21 15:15:18 +0300

    feat(auth-api): Connect all modules; add main app bootstrap configuration in main.ts.

---
+++ apps/api/auth-api/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { KyselyModuleConfigBuilder } from "@ovp-lib/api/database/kysely-module-config-builder";
+
+import { AuthModule } from "~src/auth/auth.module";
+import { AuthSessionsModule } from "~src/auth-sessions/auth-sessions.module";
+import { ConfigModule } from "~src/config/config.module";
+import { DatabaseConfig } from "~src/config/providers/database.config";
+import { PasswordsModule } from "~src/passwords/passwords.module";
+import { TokensModule } from "~src/tokens/tokens.module";
+
+@Module({
+	imports: [
+		ConfigModule,
+		new KyselyModuleConfigBuilder().setDatabaseConfigClass(DatabaseConfig).addDefaults().build(),
+		AuthModule,
+		AuthSessionsModule,
+		PasswordsModule,
+		TokensModule,
+	],
+})
+export class AppModule {}
---
+++ apps/api/auth-api/src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from "@nestjs/common";
+import { NestAppConfigBuilderFactory } from "@ovp-lib/api/config/nest-app-config-builder";
+
+import { AppModule } from "~src/app.module";
+import { AppConfig } from "~src/config/providers/app.config";
+
+const logger = new Logger("bootstrap");
+
+async function bootstrap() {
+	const configBuilder = await NestAppConfigBuilderFactory.create(AppModule);
+
+	const app = configBuilder
+		.provideConfig(AppConfig)
+		.setGlobalPrefix()
+		.addCors()
+		.addDefaults()
+		.addSwagger("Auth API Specification")
+		.build();
+
+	await app.listen(configBuilder.lookupConfigValue("port") as number);
+
+	logger.debug(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
