508c427c5342a332af2612a7d509289c8d2779f1
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-21 15:11:40 +0300

    feat(auth-api): Add migrations to create 'auth_sessions' and `refresh_tokens` database tables; generate Kysely database schema.

---
+++ apps/api/auth-api/db/migrations/20250819211425_create_auth_sessions_table.sql
@@ -0,0 +1,18 @@
+-- migrate:up
+create table if not exists auth_sessions
+(
+    id           uuid primary key                  default gen_random_uuid(),
+    user_id      uuid                     not null,
+    channel_id   uuid                     not null,
+    country_code varchar(2)                        default null,
+    country_name text                              default null,
+    city_name    text                              default null,
+    ip_address   inet                              default null,
+    user_agent   text                              default null,
+    expires_at   timestamp with time zone not null, -- this refers to ACCESS TOKEN expiration time
+    created_date timestamp with time zone not null default now(),
+    updated_date timestamp with time zone not null default now()
+);
+
+-- migrate:down
+drop table auth_sessions;
---
+++ apps/api/auth-api/db/migrations/20250819211440_create_refresh_tokens_table.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+create table if not exists refresh_tokens
+(
+    id                 uuid primary key                                              default gen_random_uuid(),
+    auth_session_id    uuid references auth_sessions (id) on delete cascade not null,
+    refresh_token_hash text                                                 not null,
+    expires_at         timestamp with time zone                             not null, -- this refers to REFRESH TOKEN expiration time
+    used_date          timestamp with time zone                                      default null,
+    created_date       timestamp with time zone                             not null default now(),
+    updated_date       timestamp with time zone                             not null default now()
+);
+
+-- migrate:down
+drop table refresh_tokens;
