a0f0c3c6d09c2c6ae695f53388b88982c4b4fa64
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-22 23:14:27 +0300

    feat(auth-api): Improve Swagger docs.

--- apps/api/auth-api/src/auth/controllers/auth.controller.ts
+++ apps/api/auth-api/src/auth/controllers/auth.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Headers, Post } from "@nestjs/common";
-import { ApiBadRequestResponse, ApiConflictResponse, ApiInternalServerErrorResponse } from "@nestjs/swagger";
+import {
+	ApiBadRequestResponse,
+	ApiConflictResponse,
+	ApiCreatedResponse,
+	ApiInternalServerErrorResponse,
+} from "@nestjs/swagger";
+import { Public } from "@ovp-lib/api/auth/decorators/public.decorator";
 import { NestErrorResponseDto } from "@ovp-lib/api/common/dto/nest-error-response.dto";
 import { RealIP } from "nestjs-real-ip";
 
 import { CreateAccountDto } from "~src/auth/dto/create-account.dto";
 import { AuthService } from "~src/auth/services/auth.service";
+import { AuthTokensDto } from "~src/tokens/dto/auth-tokens.dto";
 
 @Controller()
 export class AuthController {
@@ -17,6 +24,8 @@ export class AuthController {
   POST /auth/logout
   */
 
+	@Public()
+	@ApiCreatedResponse({ type: AuthTokensDto })
 	@ApiBadRequestResponse({ type: NestErrorResponseDto, description: "Params validation failed" })
 	@ApiConflictResponse({ type: NestErrorResponseDto, description: "User with provided email already exists" })
 	@ApiInternalServerErrorResponse({ type: NestErrorResponseDto })
