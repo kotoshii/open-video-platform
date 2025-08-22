1f60af50367c75754d4249c51af07dea075500c4
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-08-22 23:18:36 +0300

    fix: Use local JwtConfig providers.

--- apps/api/channel-api/src/config/config.module.ts
+++ apps/api/channel-api/src/config/config.module.ts
@@ -1,11 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule as NestConfigModule } from "@nestjs/config";
-import { JwtConfig } from "@ovp-apis/auth-api/dist/src/config/providers/jwt.config";
 import { JWT_CONFIG_INJECTION_TOKEN } from "@ovp-lib/api/config/constants/injection-tokens";
 
 import { AppConfig } from "~src/config/providers/app.config";
 import { DatabaseConfig } from "~src/config/providers/database.config";
 import { GrpcConfig } from "~src/config/providers/grpc.config";
+import { JwtConfig } from "~src/config/providers/jwt.config";
 
 @Global()
 @Module({
