00902e93bb00e4bcb48f694c77ede94943828f51
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-09-03 21:15:24 +0300

    refactor(lib/api): Remove unnecessary class-validation decorators; remove unnecessary DTO fields.

--- lib/api/kafka/dto/subscription-created-kafka-event-payload.dto.ts
+++ lib/api/kafka/dto/subscription-created-kafka-event-payload.dto.ts
@@ -1,23 +1,9 @@
-import { IsNotEmpty, IsString, IsUUID } from "class-validator";
-
 import { KafkaEventTypes } from "~kafka/constants/event-types";
-import { KafkaTopic } from "~kafka/constants/topic-names";
 import { BaseKafkaEventPayloadDto } from "~kafka/dto/base-kafka-event-payload.dto";
 import { SubscriptionCreatedKafkaEventPayload } from "~kafka/types/events/subscriptions";
 
 export class SubscriptionCreatedKafkaEventPayloadDto extends BaseKafkaEventPayloadDto {
-	static readonly Topic = KafkaTopic.SubscriptionEvents;
 	static readonly EventType = KafkaEventTypes.Subscriptions.SubscriptionCreated;
-
-	@IsUUID()
-	@IsString()
-	@IsNotEmpty()
-	subscriberId: string;
-
-	@IsUUID()
-	@IsString()
-	@IsNotEmpty()
-	channelId: string;
 
 	static createPayload(subscriberId: string, channelId: string): SubscriptionCreatedKafkaEventPayload {
 		return SubscriptionCreatedKafkaEventPayloadDto.buildPayload({
--- lib/api/kafka/dto/subscription-deleted-kafka-event-payload.dto.ts
+++ lib/api/kafka/dto/subscription-deleted-kafka-event-payload.dto.ts
@@ -1,23 +1,9 @@
-import { IsNotEmpty, IsString, IsUUID } from "class-validator";
-
 import { KafkaEventTypes } from "~kafka/constants/event-types";
-import { KafkaTopic } from "~kafka/constants/topic-names";
 import { BaseKafkaEventPayloadDto } from "~kafka/dto/base-kafka-event-payload.dto";
 import { SubscriptionDeletedKafkaEventPayload } from "~kafka/types/events/subscriptions";
 
 export class SubscriptionDeletedKafkaEventPayloadDto extends BaseKafkaEventPayloadDto {
-	static readonly Topic = KafkaTopic.SubscriptionEvents;
 	static readonly EventType = KafkaEventTypes.Subscriptions.SubscriptionDeleted;
-
-	@IsUUID()
-	@IsString()
-	@IsNotEmpty()
-	subscriberId: string;
-
-	@IsUUID()
-	@IsString()
-	@IsNotEmpty()
-	channelId: string;
 
 	static createPayload(subscriberId: string, channelId: string): SubscriptionDeletedKafkaEventPayload {
 		return SubscriptionDeletedKafkaEventPayloadDto.buildPayload({ subscriberId, channelId });
