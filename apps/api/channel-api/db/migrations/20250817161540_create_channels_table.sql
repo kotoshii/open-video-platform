c2bad457139d624ae5a01e586f90199b6f05640c
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-09-07 01:01:56 +0300

    feat(channel-api): Add db constraint to disallow negative subscriber_count.

--- apps/api/channel-api/db/migrations/20250817161540_create_channels_table.sql
+++ apps/api/channel-api/db/migrations/20250817161540_create_channels_table.sql
@@ -1,12 +1,13 @@
 -- migrate:up
-create table if not exists channels (
-    id uuid primary key default gen_random_uuid(),
-    user_id uuid not null,
-    name varchar(255) not null,
-    description varchar(1024) default null,
-    subscriber_count bigint not null default 0,
-    created_date timestamp with time zone not null default now(),
-    updated_date timestamp with time zone not null default now()
+create table if not exists channels
+(
+    id               uuid primary key                  default gen_random_uuid(),
+    user_id          uuid                     not null,
+    name             varchar(255)             not null,
+    description      varchar(1024)                     default null,
+    subscriber_count bigint                   not null default 0 check ( subscriber_count >= 0 ),
+    created_date     timestamp with time zone not null default now(),
+    updated_date     timestamp with time zone not null default now()
 );
 
 -- migrate:down
