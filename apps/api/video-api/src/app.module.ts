cfdab32c04fb27248b730c1abed78efa4235fe94
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-09-09 00:32:27 +0300

    feat(video-api): Register video module in the main app.

--- apps/api/video-api/src/app.module.ts
+++ apps/api/video-api/src/app.module.ts
@@ -4,12 +4,14 @@ import { KyselyModuleConfigBuilderFactory } from "@ovp-lib/api/config/builders/k
 
 import { ConfigModule } from "~src/config/config.module";
 import { DatabaseConfig } from "~src/config/providers/database.config";
+import { VideosModule } from "~src/videos/videos.module";
 
 @Module({
 	imports: [
 		ConfigModule,
 		KyselyModuleConfigBuilderFactory.create(DatabaseConfig, "postgres").addDefaults().build(),
 		JwtModuleConfigBuilderFactory.create().build(),
+		VideosModule,
 	],
 })
 export class AppModule {}
