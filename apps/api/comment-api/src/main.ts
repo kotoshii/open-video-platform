4eb7e6081d56882357e57864d76c894c806d95a6
pusuwa <40234487+kotoshii@users.noreply.github.com>
2025-09-16 20:24:44 +0300

    feat(comment-api): Set up grpc server microservice.

--- apps/api/comment-api/src/main.ts
+++ apps/api/comment-api/src/main.ts
@@ -1,8 +1,13 @@
+import { ReflectionService } from "@grpc/reflection";
 import { Logger } from "@nestjs/common";
+import { Transport } from "@nestjs/microservices";
 import { NestAppConfigBuilderFactory } from "@ovp-lib/api/config/builders/nest-app-config-builder";
+import { COMMENTS_PACKAGE_NAME } from "@ovp-proto/types/comments";
+import { ProtoPaths } from "@ovp-proto/types/utils/paths";
 
 import { AppModule } from "~src/app.module";
 import { AppConfig } from "~src/config/providers/app.config";
+import { GrpcConfig } from "~src/config/providers/grpc.config";
 
 const logger = new Logger("bootstrap");
 
@@ -10,8 +15,25 @@ async function bootstrap() {
 	const configBuilder = await NestAppConfigBuilderFactory.create(AppModule);
 
 	const appConfig = configBuilder.getProvider(AppConfig);
-	const app = configBuilder.addDefaults().addSwagger("Comment API Specification").build();
+	const grpcConfig = configBuilder.getProvider(GrpcConfig);
 
+	const app = configBuilder
+		.addDefaults()
+		.addSwagger("Comment API Specification")
+		.addMicroservice({
+			transport: Transport.GRPC,
+			options: {
+				package: COMMENTS_PACKAGE_NAME,
+				url: grpcConfig.grpcUrl,
+				protoPath: ProtoPaths.Comments,
+				onLoadPackageDefinition: (pkg, server) => {
+					new ReflectionService(pkg).addToServer(server);
+				},
+			},
+		})
+		.build();
+
+	await app.startAllMicroservices();
 	await app.listen(appConfig.port);
 
 	logger.debug(`Application is running on: ${await app.getUrl()}`);
